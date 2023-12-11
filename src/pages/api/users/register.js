859212abda8ba994cfa9ce298d6447cd2f993a4e
Vahdet Altundaş <vahdetaltundas2323@gmail.com>
2023-12-11 23:35:30 +0300

    register api update

--- src/pages/api/users/register.js
+++ src/pages/api/users/register.js
@@ -3,31 +3,28 @@ import User from "../../../models/User";
 import generateAvatar from "@/helper/generateAvatar";
 import bcrypt from "bcrypt";
 const handler = async (req, res) => {
-    await dbConnect();
-    const { username, email, password } = req.body;
-    const user = await User.findOne({ email: email });
-    if (user) {
-      res.status(400).json({ message: "User already exists" });
-      return;
-    }
-  
-    try {
-        const newUser = await new User({
-            username,
-            email,
-            password,
-            avatar: generateAvatar(username),
-          });
-      // generate salt to has password
-      const salt = await bcrypt.genSalt(10);
-      // create hash
-      newUser.password = await bcrypt.hash(newUser.password, salt);
-      await newUser.save();
-      res.status(200).json(newUser);
-      // set password to hashed
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  
-  export default handler;
+  await dbConnect();
+  const { username, email, password } = req.body;
+  const user = await User.findOne({ email: email });
+  if (user) {
+    res.status(400).json({ message: "User already exists" });
+    return;
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await new User({
+      username,
+      email,
+      password: hashedPassword,
+      avatar: generateAvatar(username),
+    });
+
+    await newUser.save();
+    res.status(200).json(newUser);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default handler;
