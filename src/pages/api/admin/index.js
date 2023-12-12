5f91e64b85e8c87783eafc376fb066205ddcf2ea
Vahdet Altundaş <vahdetaltundas2323@gmail.com>
2023-12-12 18:58:55 +0300

    admin page jwt

--- src/pages/api/admin/index.js
+++ src/pages/api/admin/index.js
@@ -1,38 +1,3 @@
-// import { SignJWT } from "jose";
-// import { NextResponse } from "next/server";
-// import { getJwtSecretKey } from "@/util/verifyJwtToken";
-
-// export async function POST(request) {
-//   const body = await request.json();
-
-//   // Make that below if condition as your own backend api call to validate user
-//   if (body.username==process.env.ADMIN_USERNAME && body.password==process.env.ADMIN_PASSWORD) {
-// const token = await new SignJWT({
-//   username: body.username,
-//   role: "admin", // Set your own roles
-// })
-//   .setProtectedHeader({ alg: "HS256" })
-//   .setIssuedAt()
-//   .setExpirationTime("30s") // Set your own expiration time
-//   .sign(getJwtSecretKey());
-
-//     const response = NextResponse.json(
-//       { success: true },
-//       { status: 200, headers: { "content-type": "application/json" } }
-//     );
-
-//     response.cookies.set({
-//       name: "token",
-//       value: token,
-//       path: "/",
-//     });
-
-//     return response;
-//   }
-
-//   return NextResponse.json({ success: false });
-// }
-
 import { getJwtSecretKey } from "@/util/verifyJwtToken";
 import cookie from "cookie";
 import { SignJWT } from "jose";
