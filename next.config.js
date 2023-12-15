b8a0d49bdff9d6fcdbb24b735ef05bdf09aa10a5
Vahdet Altundaş <vahdetaltundas2323@gmail.com>
2023-12-15 22:44:28 +0300

    next-update

--- next.config.js
+++ next.config.js
@@ -3,4 +3,20 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/api/products", // API endpoint'inizin yolu
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://vahdetaltundas-e-commerce-project.vercel.app",
+          },
+        ],
+      },
+      // Diğer API endpoint'leri için benzer ayarları ekleyebilirsiniz
+    ];
+  },
+};
